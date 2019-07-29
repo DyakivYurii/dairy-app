8aad65196e242d971753e355e0923307c582666d
Yurii <dyakivyurii@gmail.com>
2019-07-29 23:44:10 +0300

    Updates

---
+++ precache-manifest.9bb685d1c828fd6ab8534f5ba30bcbe0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8c79ef21207ca77bc98bc831f2e45ff",
+    "url": "/dairy-app/index.html"
+  },
+  {
+    "revision": "f49c8fbfc9cb70857078",
+    "url": "/dairy-app/static/css/main.d2b61a9b.chunk.css"
+  },
+  {
+    "revision": "b20e5a46a7fb8f51c8f9",
+    "url": "/dairy-app/static/js/2.aff167c4.chunk.js"
+  },
+  {
+    "revision": "f49c8fbfc9cb70857078",
+    "url": "/dairy-app/static/js/main.e9d861ab.chunk.js"
+  },
+  {
+    "revision": "e574ec4ab21f0fe4773b",
+    "url": "/dairy-app/static/js/runtime~main.2a02b4b7.js"
+  }
+]);
