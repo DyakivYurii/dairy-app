dbbf36df88510fde1830071197616791d9743382
Yurii <dyakivyurii@gmail.com>
2019-07-29 23:34:44 +0300

    simple changes to app

---
+++ build/precache-manifest.7b2a4d58b2b36b9377583d6cb505bc69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0c770845235c047170cb387c1dcff0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b383c446e5464ce8ebc1",
+    "url": "/static/css/main.d2b61a9b.chunk.css"
+  },
+  {
+    "revision": "b20e5a46a7fb8f51c8f9",
+    "url": "/static/js/2.aff167c4.chunk.js"
+  },
+  {
+    "revision": "b383c446e5464ce8ebc1",
+    "url": "/static/js/main.d403e71c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
